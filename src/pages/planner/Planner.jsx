6337a4d85d4ab1215b720fad8abd7c2ce895e0d9
Akhil K Thomas <62604580+akhilkurisinkal@users.noreply.github.com>
2024-06-15 01:23:12 +0000

    update 2

--- src/pages/planner/Planner.jsx
+++ src/pages/planner/Planner.jsx
@@ -1,42 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Planner.module.css";
 import PlannerChild from "../../components/plannerChild/PlannerChild";
-import NutritionDash from "../../components/nutritionDash/NutritionDash";
 
-const Planner=()=>{
+const Planner = () => {
+    const [foodItem, setFoodItem] = useState({ fname: "", fcal: "" });
 
-    const [breakfast,setBreakfast]={};
-    const [morningSnack,setMorningSnack]={};
-    const [lunch,setLunch]={};
-    const [eveningSnack,setEveningSnack]={};
-    const [dinner,setDinner]={};
+    const handleChangeFname = (event) => {
+        setFoodItem({ ...foodItem, fname: event.target.value });
+        console.log(foodItem.fname);
+    };
 
-    const set
-
-    return(
+    return (
         <div className={styles.planner}>
-
             <h3>Create meal plan</h3>
 
-      
             <PlannerChild title="Breakfast" calories="390" />
-            <PlannerChild title="Morning snacks" calories="200"/>
-            <PlannerChild title="Lunch" calories="550"/>
-            <PlannerChild title="Evening snacks" calories="200"/>
-            <PlannerChild title="Dinner" calories="680"/>
-            
-            <div>
-                this is a popup for adding meals
-                <h4>Add meal</h4>
-                <label htmlFor="mealName">Name</label>
-                <input type="text" name="mealName" id="mealName" /><br/>
-                <label htmlFor="cal">Calories</label>
-                <input type="text" name="cal" id="cal"/><br/>
-                <button onClick={}>Add meal</button>
-            </div>
+            <PlannerChild title="Morning snacks" calories="200" />
+            <PlannerChild title="Lunch" calories="550" />
+            <PlannerChild title="Evening snacks" calories="200" />
+            <PlannerChild title="Dinner" calories="680" />
 
+            <div>
+                this is a popup for adding food Items to create a meal
+                <h4>Add food</h4>
+                <label htmlFor="foodItemName">Name</label>
+                <input
+                    type="text"
+                    name="foodItemName"
+                    id="foodItemName"
+                    value={foodItem.fname}
+                    onChange={handleChangeFname}
+                />
+                <br />
+                <label htmlFor="cal">Calories</label>
+                <input type="text" name="cal" id="cal" />
+                <br />
+                <button>Add meal</button>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Planner;
